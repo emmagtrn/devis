21e53b4390906c07017a6dc6f24d77c26be7dcd2
clovi <clovi@172.17.17.124>
2026-05-20 09:22:24 +0200

    "Initialisation du projet : Interface, Dessin 2D et Murs"

--- src/main/java/fr/insa/gaspardclovisemma/modele/Appartement.java
+++ src/main/java/fr/insa/gaspardclovisemma/modele/Appartement.java
@@ -3,16 +3,22 @@ package fr.insa.gaspardclovisemma.modele;
 import java.util.ArrayList;
 import java.util.List;
 
+// Représente un foyer/logement spécifique sur un palier (ex: Appartement 101)
 public class Appartement {
     private int idAppartement;
+    
+    // Suite de l'emboîtement : Un appartement contient une liste de pièces (Salon, Cuisine...)
     private List<Piece> pieces = new ArrayList<>();
 
+    // Constructeur
     public Appartement(int idAppartement) {
         this.idAppartement = idAppartement;
     }
 
+    // Méthode pour ajouter une nouvelle pièce au logement
     public void ajouterPiece(Piece p) { pieces.add(p); }
-    public List<Piece> getPieces() { return pieces; }
     
+    // Getters
+    public List<Piece> getPieces() { return pieces; }
     public int getId() { return idAppartement; }
 }
